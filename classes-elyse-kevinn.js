75ccfca934f0c914766b6cbf9fcc4a8ba6f58a92
kevinnarbas <kevinnarbas@gmail.com>
2021-02-17 16:09:05 -0800

    challenge 2 completed

--- classes-elyse-kevinn.js
+++ classes-elyse-kevinn.js
@@ -2,53 +2,70 @@
 
 
 // Coffee Maker
-class Coffee {
-  constructor(type, cream, sugar){
-    this.type = type.toLowerCase()
-    this.cream = cream
-    this.sugar = sugar
-  }
+// class Coffee {
+//   constructor(type, cream, sugar){
+//     this.type = type.toLowerCase()
+//     this.cream = cream
+//     this.sugar = sugar
+//   }
 
-  coffeeProfile(){
-    return(`${this.type}: ${this.creams()}, ${this.sugars()}`)
-  }
+//   coffeeProfile(){
+//     return(`${this.type}: ${this.creams()}, ${this.sugars()}`)
+//   }
 
-  creams(){
-    if (this.cream > 1){
-      return `${this.cream} creams`
-    } else {
-      return `${this.cream} cream`
-    }
-  }
+//   creams(){
+//     if (this.cream > 1){
+//       return `${this.cream} creams`
+//     } else {
+//       return `${this.cream} cream`
+//     }
+//   }
 
-  sugars(){
-    if (this.sugar > 1){
-      return `${this.sugar} sugars`
-    } else {
-      return `${this.sugar} sugar`
-    }
-  }
-}
+//   sugars(){
+//     if (this.sugar > 1){
+//       return `${this.sugar} sugars`
+//     } else {
+//       return `${this.sugar} sugar`
+//     }
+//   }
+// }
 // Write the code that makes a black coffee.
-let blackCoffee = new Coffee("black",0,0)
-console.log(blackCoffee);
+// let blackCoffee = new Coffee("black",0,0)
+// console.log(blackCoffee);
 //
 // Write the code that makes a coffee with 1 cream and 2 sugars.
-let coffeeOne = new Coffee("blonde",1,2)
-console.log(coffeeOne);
+// let coffeeOne = new Coffee("blonde",1,2)
+// console.log(coffeeOne);
 //
 // Write the code that makes a coffee with 2 sugars. Then write the code that outputs the coffee's profile.
-let darkCoffee = new Coffee("dark",0,2)
-console.log(darkCoffee.coffeeProfile())
+// let darkCoffee = new Coffee("dark",0,2)
+// console.log(darkCoffee.coffeeProfile())
 //
 // Latte Maker
 // Write a Latte class that receives a flavor, a milk type and a number of shots.
+class Latte {
+  constructor(flavor, milkType, shots) {
+    //     key  :  value
+    this.flavor = flavor;
+    this.milkType = milkType;
+    this.shots = shots;
+  }
+
+  latteProfile(){
+    return (`${this.flavor}: ${this.milkType}, ${this.shots}`)
+  }
+}
+
 //
 // Write a method for your Latte class that outputs the latte's profile.
 //
 // Write the code that makes a regular, single shot latte. Then, log the latte's profile.
+let regLatte = new Latte('regular', 'cow', 1)
+console.log(regLatte.latteProfile())
 //
 // Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
+let hazelnutLatte = new Latte('hazelnut', 'almond', 2)
+console.log(hazelnutLatte.latteProfile())
 //
 // Volume of a Cylinder
 // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
