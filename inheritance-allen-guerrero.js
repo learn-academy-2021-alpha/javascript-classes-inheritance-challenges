d19d1ccc62eaf2b94ddc17a0b9174f87e6d83ecb
guerrerocampos <jrheezy.gc@gmail.com>
2021-02-18 10:33:18 -0800

    finished challenge 14

--- inheritance-allen-guerrero.js
+++ inheritance-allen-guerrero.js
@@ -9,6 +9,15 @@ class Car {
         this.model = model;
         // The year for the car class can be "myCar year"
         this.year = year;
+        // give all my cars a lights property. Lights start in the off position.
+        this.lights = 'off';
+    }
+    headLights () {
+        if (this.lights === 'off') {
+            this.lights = 'on'
+        } else if(this.lights === 'on') {
+            this.lights = 'off'
+        }
     }
     // Calling the method wheels will return 4
     wheels() {
@@ -34,11 +43,24 @@ class Toyota extends Car {
         super(model, year);
     }
 }
+// make a Volkswagen car.
+// class Volkswagen inherits from class Car
+// create an object called myVolkswagen which is a instance of class Volkswagen
+class Volkswagen extends Car {
+    constructor(model, year) {
+        super(model, year);
+    }
+}
+
 
 // Create variable that stores the object 'myCar', which is an instance of the class Car
 var myCar = new Car();
 var myTesla = new Tesla('Tesla', 2021)
 var allensToyota = new Toyota('Prius', 2015)
+var guerrerosVolkswagen = new Volkswagen('jetta', 2012)
 console.log(myCar.wheels());
 console.log(myTesla)
+allensToyota.headLights()
+allensToyota.headLights()
 console.log(allensToyota)
+console.log(guerrerosVolkswagen)
