255b927c98a77fc2c9e916775b96f5601263d86a
guerrerocampos <jrheezy.gc@gmail.com>
2021-02-18 10:05:30 -0800

    challenge four complete

--- inheritance-allen-guerrero.js
+++ inheritance-allen-guerrero.js
@@ -7,10 +7,27 @@ class Car {
         // As a programmer, I can give my car a model on initialization.
         // The model for the car class can be "generic car"
         this.model = 'generic car';
+        // The year for the car class can be "myCar year"
+        this.year = 'myCar year'
+    }
+    // Calling the method wheels will return 4
+    wheels() {
+        this.myWheels = 4
+        return this.myWheels
+    }
+
+}
+    // make a Tesla car.
+// class Tesla inherits from class Car
+// Create an object called myTesla which is a instance of class Tesla
+class Tesla extends Car {
+    constructor() {
+        super(); 
     }
 }
 
 // Create variable that stores the object 'myCar', which is an instance of the class Car
 var myCar = new Car();
-
-console.log(myCar);
+var myTesla = new Tesla()
+console.log(myCar.wheels());
+console.log(myTesla)
