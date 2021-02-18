4c4b812759994a249cc641357db7e159687f01fb
algtan <agtan@ymail.com>
2021-02-18 09:54:11 -0800

    challenge 1 and 2 complete

---
+++ inheritance-allen-guerrero.js
@@ -0,0 +1,16 @@
+// Challenge 1
+// Story: As a programmer, I can make a car.
+
+// Create a class called 'Car'
+class Car {
+    constructor() {
+        // As a programmer, I can give my car a model on initialization.
+        // The model for the car class can be "generic car"
+        this.model = 'generic car';
+    }
+}
+
+// Create variable that stores the object 'myCar', which is an instance of the class Car
+var myCar = new Car();
+
+console.log(myCar);
